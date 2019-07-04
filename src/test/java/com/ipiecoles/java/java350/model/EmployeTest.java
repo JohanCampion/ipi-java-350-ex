cdee50627efe4be7593a8ab66b73d1d1a06b5f91
johan campion <johan-externe.campion@edf.fr>
2019-07-04 11:07:15 +0200

    c'est mieux avec les exercices

--- src/test/java/com/ipiecoles/java/java350/model/EmployeTest.java
+++ src/test/java/com/ipiecoles/java/java350/model/EmployeTest.java
@@ -1,5 +1,6 @@
 package com.ipiecoles.java.java350.model;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -84,5 +85,94 @@ public class EmployeTest {
         Assertions.assertEquals(primeAnnuelle, prime);
 
     }
+    @Test
+    public void augmenterSalaire50() {
+        //Given
+        Employe employe = new Employe();
+        employe.setSalaire(1256.00);
+
+        //When
+        employe.augmenterSalaire(50.00);
+
+        //Then
+        Assertions.assertEquals(1884.00,employe.getSalaire().doubleValue());
+
+    }
+    @Test
+    public void augmenterSalaire0() {
+        //Given
+        Employe employe = new Employe();
+        employe.setSalaire(1500.00);
+
+        //When
+        employe.augmenterSalaire(0);
+
+        //Then
+        Assertions.assertEquals(1500,employe.getSalaire().doubleValue());
+
+    }
+    @Test
+    public void augmenterSalaire(){
+        //Given
+        Employe employe = new Employe();
+        employe.setSalaire(100.00);
+
+        //When
+        employe.augmenterSalaire(2.60);
+
+        //Then
+        Assertions.assertEquals(102.60,employe.getSalaire().doubleValue());
+
+    }
+
+    @Test
+    public void augmenterSalaireNull(){
+        //Given
+        Employe employe = new Employe();
+        employe.setSalaire(null);
+        //When
+        employe.augmenterSalaire(50);
+
+        //Then
+        Assertions.assertEquals(0,employe.getSalaire().doubleValue());
+    }
+
+    @Test
+    public void augmenterSalaireWithNegativeParameter(){
+        //Given
+        Employe employe = new Employe();
+        employe.setSalaire(1500.00);
+
+        //When
+        IllegalArgumentException e = Assertions.assertThrows(IllegalArgumentException.class,() -> employe.augmenterSalaire(-15.5));
+
+        //Then
+        Assert.assertEquals("Le pourcentage d'augmentation ne peut pas être négative !",e.getMessage());
+
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "2019-01-01,1,8",
+            "2021-01-01,1,10",
+            "2022-01-01,1,10",
+            "2022-01-01,0.5,5",
+            "2032-01-01,1,11"
+    })
+    public void getNbRTT(LocalDate d,double tempPartiel,int RttExpect){
+        //Given
+        Employe employe = new Employe("Doe", "Jhon", "T00001", LocalDate.now(),Entreprise.SALAIRE_BASE, 1,tempPartiel);
+
+        //When
+        int RttCalculated = employe.getNbRtt(d);
+
+        //Then
+        Assertions.assertEquals(RttCalculated, RttExpect);
+
+    }
+
+
+
+
 
 }
